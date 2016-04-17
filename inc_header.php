fd5edd90ecf209f7215dba33e8228d93a79d8c39
Roberto Nicoletti <roberto.urias@gmail.com>
2016-04-17 17:48:55 -0300

    Alteração da imagem principal

--- inc_header.php
+++ inc_header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <img class="img-responsive img-profile" src="img/perfil/Roberto-Nicoletti.jpg" alt="">
+                <img class="img-responsive img-profile" src="img/perfil/Roberto.jpg" alt="">
                 <div class="intro-text">
                     <span class="name">Roberto Nicoletti</span>
                     <hr class="star-light">
